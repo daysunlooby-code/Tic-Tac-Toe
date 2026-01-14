3c6e1c93c94d8641e41082f7aa5a9ce442aac93d
daysunlooby-code <daysunlooby@gmail.com>
2026-01-13 19:22:45 -0500

    Update Iteration 4.py

--- Iteration 4.py
+++ Iteration 4.py
@@ -1,1 +1,7 @@
 
+def place_move(board, choice, player_symbol):
+for row in range(len(board)):
+for col in range(len(board[row])):
+if board[row][col] == choice:
+board[row][col] = player_symbol
+return board
