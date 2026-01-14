1b683d6a4a34235ea5ee3a64e13f9ae6d6386df9
daysunlooby-code <daysunlooby@gmail.com>
2026-01-13 19:26:11 -0500

    Update Iteration 6.py

--- Iteration 6.py
+++ Iteration 6.py
@@ -1,1 +1,16 @@
+def check_winner(board, player_symbol):
+for row in board:
+if all(cell == player_symbol for cell in row):
+return True
 
+for col in range(3):
+if all(board[row][col] == player_symbol for row in range(3)):
+return True
+
+if all(board[i][i] == player_symbol for i in range(3)):
+return True
+
+if all(board[i][2 - i] == player_symbol for i in range(3)):
+return True
+
+return False
